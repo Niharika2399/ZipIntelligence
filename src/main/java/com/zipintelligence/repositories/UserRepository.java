fbd4e4ec493d3b74db5ead008402fed0af173c86
learnalltheway <74134952+learnalltheway@users.noreply.github.com>
2024-10-04 23:22:28 -0400

    Delete src/main/java/com/zipintelligence/repositories directory

--- src/main/java/com/zipintelligence/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zipintelligence.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zipintelligence.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
